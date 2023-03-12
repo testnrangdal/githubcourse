24d2f37c8421dcdf28c9d52ddbf3b22933e7d0a7
testnrangdal <nageshwari.rangdal@gmail.com>
2023-03-12 17:25:47 +0530

    added/updated/deleted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -3,6 +3,9 @@ package pages;
 public class LoginPage {
 	public static void main(String a[]) {
 		System.out.println("Hello loginPage");
+		String un = "admin";
+		String pwd = "admin";
+		System.out.println("logged in...");
 		
 	}
 
---
+++ src/pages/Searchpage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class Searchpage {
+	
+	public void search() {
+		System.out.println("search");
+		
+	}
+
+}
